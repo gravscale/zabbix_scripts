cfe1235c6158f7b562b0f44877a192ef1662fd13
Roberto Berto <roberto.berto@gmail.com>
2021-06-15 13:34:51 -0300

    removed ./ from tmp

--- zabbix_ospf_routes_number_install.sh
+++ zabbix_ospf_routes_number_install.sh
@@ -13,8 +13,10 @@ cat << EOF > /tmp/zabbix_ospf_routes_number_cron.sh
 source /opt/vyatta/etc/functions/script-template
 set system task-scheduler task zabbix_ospf_routes_number executable path /usr/local/sbin/get_zabbix_ospf_routes_number
 set system task-scheduler task zabbix_ospf_routes_number crontab-spec "* * * * *"
+commit
+save
 EOF
 
 chmod 0700 /tmp/zabbix_ospf_routes_number_cron.sh
-./tmp/zabbix_ospf_routes_number_cron.sh
+/tmp/zabbix_ospf_routes_number_cron.sh
 rm -f /tmp/zabbix_ospf_routes_number_cron.sh
